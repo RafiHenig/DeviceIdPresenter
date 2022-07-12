2c765f499e0027b36bc8f573ea4e20e7f5b4ff5a
רפאל הניג <yesrefael@gmail.com>
2022-07-12 16:07:30 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DeviceId;
+
+var key = new DeviceIdBuilder().AddMachineName()
+                               .AddOsVersion()
+                               .OnWindows(windows => windows.AddProcessorId()
+                                                            .AddMotherboardSerialNumber()
+                                                            .AddSystemDriveSerialNumber()
+
+                               )
+                               .ToString(); 
+Console.WriteLine("Unique key: {0}: ",key);
+Console.ReadLine();
